d5de3d425ae2464255afea2b44b89e33e81fddbe
akadahiroyasu <39513408+akadahiroyasu@users.noreply.github.com>
2019-08-01 08:57:39 -0700

    change

--- djangoai.master/aiapps/carbike/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name ='carbike'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('predict/', views.predict, name='predict'),
-]
